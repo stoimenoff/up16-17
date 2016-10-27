c4efc8f6ba8ede80e52ce5c8dad2c503cf765b83
stoimenoff <stoimenoffjr@gmail.com>
2016-10-28 01:31:09 +0300

    Update week3 practicum cards task solution

--- week3/practicum/solutions/task4.cpp
+++ week3/practicum/solutions/task4.cpp
@@ -1,59 +1,57 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main()
+{
 	int card_number;
 	cout << "Enter card number: ";
 	cin >> card_number;
-	int card_value = card_number % 13;
-	char symbol;
-	if (card_number >= 1 && card_number <= 13)
+	
+	if(card_number < 1 || card_number > 52)
 	{
-		symbol = 'C';
+		cout << "Ivalid card number!" << endl;
+		return 0; //stop main() function
 	}
-	else if (card_number >= 14 && card_number <= 26)
+
+	int card_value = ((card_number - 1) % 13) + 2;
+	if(card_value >= 2 && card_value <= 10) //is a number card
 	{
-		symbol = 'D';
+		cout << card_value << " ";
 	}
-	else if (card_number >= 27 && card_number <= 39)
+	else //not a number card
 	{
-		symbol = 'H';
+		switch (card_value)
+		{
+			case 11:
+				cout << "J ";
+				break;
+			case 12:
+				cout << "Q ";
+				break;
+			case 13:
+				cout << "K ";
+				break;
+			case 14: 
+				cout << "A ";
+				break;
+		}
 	}
-	else if (card_number >= 40 && card_number <= 52)
+
+	int card_color = ((card_number - 1) / 13) + 1;
+	switch(card_color)
 	{
-		symbol = 'S';
-	}
-	else
-	{
-		cout << "Invalid card number..!" << endl;
-		return 0; // stopping main() function
-	}
-	switch (card_value)
-	{
-		case 0: 
-			cout << "A " << symbol << endl;
+		case 1: 
+			cout << "Clubs" << endl;
 			break;
-		case 1:
 		case 2:
+			cout << "Diamonds" << endl;
+			break;
 		case 3:
+			cout << "Hearts" << endl;
+			break;
 		case 4:
-		case 5:
-		case 6:
-		case 7:
-		case 8:
-		case 9: 
-			cout << card_value + 1 << " " << symbol << endl;
+			cout << "Spades" << endl;
 			break;
-		case 10: 
-			cout << "J " << symbol << endl;
-			break;
-		case 11:
-			cout << "Q " << symbol << endl;
-			break;
-		case 12:
-			cout << "K " << symbol << endl;
-			break;
-		default:
-			cout << "Invalid data..!" << endl;
 	}
 	return 0;
 }
