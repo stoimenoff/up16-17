995ff69888acce34ae3bfbb8f0259ca2e84225df
stoimenoff <stoimenoffjr@gmail.com>
2016-11-25 15:48:40 +0200

    Rename hw1 solutions files and add extra comments to some tasks

--- homeworks_solutions/1/Task-9.cpp
+++ homeworks_solutions/1/Task-9.cpp
@@ -19,8 +19,16 @@ int main()
 	unsigned int firstOperand = 0, secondOperand = 0, result = 0;
 	char operation;
 	cin >> firstOperand >> operation >> secondOperand;
-	firstOperand %= (1 << BITS); // Simulate the overflow.
+	/* Симулираме препълването като взимаме остатъкът при деление на 16
+		* 16, защото калкулаторът е 4 битов, т.е. имаме 2^4 числа без знак
+	*/
+	firstOperand %= (1 << BITS);
 	secondOperand %= (1 << BITS);
+
+	/* Първо се "отърваваме" от делението на нула, като записваме в резултата -1,
+		което ще е сигнал, именно за това.
+		След като сме сигурни, че не делим на нула, извършваме съответната операция
+	*/
 	if(secondOperand == 0 && (operation == '/' || operation == '%'))
 		result = -1;
 	else if (operation == '+')
@@ -37,6 +45,7 @@ int main()
 	if(result == -1)
 		cout << "Division by zero!" << endl;
 	else
+		// Преди да изведем резултата, отново симулираме препълването
 		cout << (result % (1 << BITS)) << endl;
 	return 0;
 }
